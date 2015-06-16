9f5b0ed43c0b1590c9d3a8655030a43a29291a3e
Zeqing Zhang <zeqingzhang@Zeqings-MacBook-Pro.local>
2015-06-15 21:09:24 -0400

    Initial Commit

---
+++ zzq/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  zzq
+//
+//  Created by Zeqing Zhang on 6/15/15.
+//  Copyright (c) 2015 Zeqing Zhang. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
